6195324b7f1b37f1e41b842b7dfcce86249e172b
Takatoshi Matsumoto <toqoz403@gmail.com>
2013-08-14 14:03:25 +0900

    Fix static filepath in router

--- gokuraku/router/router_methods.go
+++ gokuraku/router/router_methods.go
@@ -5,6 +5,7 @@ import (
 	"github.com/ToQoz/Gokuraku/gokuraku/controllers"
 	"github.com/stretchr/goweb"
 	"github.com/stretchr/goweb/context"
+	"os"
 )
 
 func Map() {
@@ -13,10 +14,12 @@ func Map() {
 }
 
 func mapStatics() {
-	goweb.MapStaticFile("/", "public/html/index.html")
-	goweb.MapStatic("/js", "public/js")
-	goweb.MapStatic("/css", "public/css")
-	goweb.MapStaticFile("/favicon.ico", "public/favicon.ico")
+	root := os.Getenv("GOPATH") + "/src/github.com/ToQoz/Gokuraku"
+
+	goweb.MapStaticFile("/", root+"/public/html/index.html")
+	goweb.MapStatic("/js", root+"/public/js")
+	goweb.MapStatic("/css", root+"/public/css")
+	goweb.MapStaticFile("/favicon.ico", root+"/public/favicon.ico")
 	goweb.Map("/soundcloud_client_id", func(ctx context.Context) error {
 		return goweb.API.RespondWithData(ctx, gokuraku.Config.SoundcloudClientId)
 	})
